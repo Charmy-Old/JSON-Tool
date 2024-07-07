3bb5de300e1112a73ebd441e4a3410b13adbbffd
Charmy <charmytseng0118@gmail.com>
2024-07-07 17:46:36 +0800

    修改如果 input 資料已經是完整的 JSON 格式就不要再加 {}

--- javascript/organize.js
+++ javascript/organize.js
@@ -1,21 +1,18 @@
 function organize(inputData) {
-  const expectedData = {};
-  const organizingData = inputData.split(',').map(pair => pair.trim());
+  let expectedData;
 
-  organizingData.forEach(pair => {
-    const [key, value] = pair.split(':').map(part => part.trim().replace(/['"]/g, ''));
-    expectedData[key] = value;
-  });
-  
-  let resultText = '{\n';
-  Object.keys(expectedData).forEach((key, index, array) => {
-    resultText += `"${key}": "${expectedData[key]}"`;
-    if (index < array.length - 1) {
-      resultText += ',';
-    }
-    resultText += '\n';
-  });
-  resultText += '}';
-  
+  try {
+    expectedData = JSON.parse(inputData);
+  } catch (e) {
+    expectedData = {};
+    const organizingData = inputData.split(',').map(pair => pair.trim());
+
+    organizingData.forEach(pair => {
+      const [key, value] = pair.split(':').map(part => part.trim().replace(/['"]/g, ''));
+      expectedData[key] = value;
+    });
+  }
+
+  const resultText = JSON.stringify(expectedData, null, 2);
   document.getElementById('resultBlock').value = resultText;
 }
